9b3719c2e06cc4aa0f6f746a3e64aad096e0c62c
fxsharafuddin904-pixel <fxsharafuddin904@gmail.com>
2026-07-16 20:03:16 +0600

    Update database.js

--- database.js
+++ database.js
@@ -1,1 +1,71 @@
+const fs = require("fs");
 
+const DB_FILE = "./database.json";
+
+// Database তৈরি
+function loadDatabase() {
+    if (!fs.existsSync(DB_FILE)) {
+        fs.writeFileSync(DB_FILE, JSON.stringify({
+            users: {}
+        }, null, 2));
+    }
+
+    return JSON.parse(fs.readFileSync(DB_FILE));
+}
+
+// Save Database
+function saveDatabase(data) {
+    fs.writeFileSync(DB_FILE, JSON.stringify(data, null, 2));
+}
+
+// User Create
+function getUser(id) {
+
+    const db = loadDatabase();
+
+    if (!db.users[id]) {
+
+        db.users[id] = {
+
+            timeframe: 1,
+            market: null,
+            step: "idle"
+
+        };
+
+        saveDatabase(db);
+
+    }
+
+    return db.users[id];
+
+}
+
+// Update User
+function updateUser(id, data) {
+
+    const db = loadDatabase();
+
+    db.users[id] = {
+
+        ...getUser(id),
+        ...data
+
+    };
+
+    saveDatabase(db);
+
+}
+
+// Get All
+function getDatabase() {
+    return loadDatabase();
+}
+
+module.exports = {
+
+    getUser,
+    updateUser,
+    getDatabase
+
+};
