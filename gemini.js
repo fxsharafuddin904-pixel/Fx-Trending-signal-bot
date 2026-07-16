e0f04a30fe3f550cc32759b789eaa14200d4f51e
fxsharafuddin904-pixel <fxsharafuddin904@gmail.com>
2026-07-16 22:44:47 +0600

    Update gemini.js

--- gemini.js
+++ gemini.js
@@ -6,8 +6,7 @@ const config = require("./config");
 ========================================= */
 
 const GEMINI_URL =
-`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${config.GEMINI_API_KEY}`;
-
+`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${config.GEMINI_API_KEY}`;
 /* =========================================
    DOWNLOAD IMAGE
 ========================================= */
