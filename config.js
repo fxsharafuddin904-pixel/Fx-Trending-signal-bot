e730ade9018472b10d9fcb3c93bd959e1fa23f75
fxsharafuddin904-pixel <fxsharafuddin904@gmail.com>
2026-07-16 20:02:47 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,21 @@
+module.exports = {
+  BOT_TOKEN: process.env.BOT_TOKEN,
+  GEMINI_API_KEY: process.env.GEMINI_API_KEY,
 
+  BOT_NAME: "📈 AI Trading Signal Bot",
+
+  MARKETS: [
+    "EUR/USD",
+    "USD/JPY",
+    "GBP/USD",
+    "AUD/USD",
+    "USD/CAD",
+    "USD/CHF",
+    "NZD/USD",
+    "EUR/JPY",
+    "EUR/GBP",
+    "GBP/JPY"
+  ],
+
+  TIMEFRAMES: [1, 2, 3, 4, 5]
+};
