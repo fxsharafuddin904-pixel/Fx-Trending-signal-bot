40fa4cb9997aa528dc79e6e3e2b21a4a8972f131
fxsharafuddin904-pixel <fxsharafuddin904@gmail.com>
2026-07-17 11:27:55 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   BOT_TOKEN: "8747004115:AAFCna-PeLWlEqEhGV5yjRpBwPGjyQD7fOA",
-  GEMINI_API_KEY: "AIzaSyAQVdB3tjldTj2o3xn6rS7FbfCLsvg2RcU",
+  GEMINI_API_KEY: "AIzaSyBB8EL4maD9bOZ1bqIHPF7rufJo_V-h0D8",
 
   BOT_NAME: "📈 AI Trading Signal Bot",
 
